7cb1ff85fbac2ae0806cdc465c1819512a7d3dfc
hosseinchb <Hossein.chasbavi@gmail.com>
2025-02-14 18:54:01 +0330

    feat: get api for getting prediction added.

--- forcast/urls.py
+++ forcast/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PredictionCreateView
+from .views import PredictionCreateView, PredictionByInstrumentView
 
 urlpatterns = [
     path('CreatePredictions/', PredictionCreateView.as_view(), name='prediction-create'),
+    path('GetPredictionsByInstrumentIdAndDays/<int:instrument_id>/<int:days>/', PredictionByInstrumentView.as_view(), name='prediction-by-instrument-and-days'),    
 ]
--- forcast/views.py
+++ forcast/views.py
@@ -17,3 +17,37 @@ class PredictionCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import datetime, timedelta
+from .models import Prediction
+from .serializers import PredictionSerializer
+
+class PredictionByInstrumentView(APIView):
+    def get(self, request, instrument_id, days):
+        try:
+            # Validate input
+            days = int(days)
+            if days < 0:
+                return Response({"error": "Days must be non-negative"}, status=status.HTTP_400_BAD_REQUEST)
+
+            instrument_id = int(instrument_id)
+            if instrument_id < 0:
+                return Response({"error": "instrument_is must be non-negative"}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Get today's date and calculate the range
+            today = datetime.today().date()
+            end_date = today + timedelta(days=days)
+
+            # Fetch predictions within the date range
+            predictions = Prediction.objects.filter(instrument_id=instrument_id, date__range=[today, end_date])
+            
+            # Serialize the data
+            serializer = PredictionSerializer(predictions, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
